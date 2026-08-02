54e3391028a303e078757acc99bf21c489e7c42c
Flapchik <58729268+Flapchik@users.noreply.github.com>
2026-08-02 21:14:36 +0300

    fix (bot): удалить неиспользуемый роутер recipes

--- backend/bot/handlers/__init__.py
+++ backend/bot/handlers/__init__.py
@@ -1,5 +1,4 @@
 from .other import router as other_router
-from .recipes import router as recipes_router
 from .recognize import router as recognize_router
 from .start import router as start_router
 
@@ -7,6 +6,5 @@ from .start import router as start_router
 routers = [
     start_router,
     recognize_router,
-    recipes_router,
     other_router
 ]
