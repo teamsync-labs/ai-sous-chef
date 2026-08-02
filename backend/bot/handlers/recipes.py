3dfd4f6301d4212e613cc07884b9595351186632
Flapchik <58729268+Flapchik@users.noreply.github.com>
2026-08-02 21:13:19 +0300

    feat (bot): удалить неиспользуемый функционал get_recipes_cmd

--- backend/bot/handlers/recipes.py
+++
@@ -1,9 +0,0 @@
-from aiogram import Router, F
-from aiogram.types import Message
-
-router = Router()
-
-
-@router.message(F.text)
-async def get_recipes_cmd(message: Message):
-    await message.reply(f"Был отправлен текст: {message.text}")
