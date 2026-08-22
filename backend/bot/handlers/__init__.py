2436f9b40831092b7c8e241ea0406314e255a58c
Flapchik <58729268+Flapchik@users.noreply.github.com>
2026-08-22 11:48:22 +0300

    feat (bot): добавить хендлер delete для удаления consents

--- backend/bot/handlers/__init__.py
+++ backend/bot/handlers/__init__.py
@@ -1,10 +1,12 @@
 from .other import router as other_router
 from .recognize import router as recognize_router
 from .start import router as start_router
+from .delete import router as delete_router
 
 
 routers = [
     start_router,
+    delete_router,
     recognize_router,
     other_router
 ]
