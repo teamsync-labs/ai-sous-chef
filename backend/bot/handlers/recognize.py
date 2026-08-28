44f01cd680a7318760ed86d5686b67b16583d038
Flapchik <58729268+Flapchik@users.noreply.github.com>
2026-08-28 20:19:04 +0300

    feat (bot): добавить диалог при распознавании продуктов с фото/документа

--- backend/bot/handlers/recognize.py
+++ backend/bot/handlers/recognize.py
@@ -5,7 +5,7 @@ from aiogram.fsm.context import FSMContext
 from aiogram.types import Message, CallbackQuery
 
 from keyboards.product_list import ProductListCallback, keyboard_approve_products_builder
-from keyboards.recipes_list import keyboard_recipes_builder, RecipesListCallback
+from keyboards.recipes_list import keyboard_recipes_builder, RecipesListCallback, keyboard_recipe_back_builder
 from services import api_client
 from states.recognize_states import RecognizeState
 
@@ -22,10 +22,11 @@ async def get_recognize_photo_cmd(message: Message, state: FSMContext):
     image_b64 = base64.b64encode(file_bytes.read()).decode('utf-8')
     result = await api_client.recognize(base64=image_b64)
 
-    await state.set_state(None)
     await state.set_state(RecognizeState.waiting_for_product_list_approval)
+    await state.update_data({"products": result.get("products", {})})
 
-    await message.reply(f"Было отправлено фото. Результат {result}")
+    await message.reply(f"Мы распознали такие продукты. {', '.join(result.get('products'))}\n"
+                        f"Всё верно?", reply_markup=keyboard_approve_products_builder())
 
 
 @router.message(F.document)
@@ -39,6 +40,7 @@ async def get_recognize_photo_cmd(message: Message, state: FSMContext):
     result = await api_client.recognize(base64=image_b64)
 
     await state.set_state(RecognizeState.waiting_for_product_list_approval)
+    await state.update_data({"products": result.get("products", {})})
 
     await message.reply(f"Мы распознали такие продукты. {', '.join(result.get('products'))}\n"
                         f"Всё верно?", reply_markup=keyboard_approve_products_builder())
@@ -46,8 +48,7 @@ async def get_recognize_photo_cmd(message: Message, state: FSMContext):
 
 @router.message(F.text)
 async def get_recognize_text_cmd(message: Message, state: FSMContext):
-    # result = await api_client.recognize(text=message.text)
-    result = {"products": ["мёд", "чечевица", "масло"]}
+    result = await api_client.recognize(text=message.text)
 
     await state.set_state(RecognizeState.waiting_for_product_list_approval)
     await state.update_data({"products": result.get("products", {})})
@@ -73,7 +74,6 @@ async def get_approval_product_list(cb: CallbackQuery, callback_data: ProductLis
 
     result = await api_client.get_recipes(products)
     await state.update_data({"recipes": result.get("recipes", [])})
-    # {'recipes': [{'title': 'Чечевичная каша с мёдом', 'steps': ['Промойте чечевицу под проточной водой.', 'Вскипятите воду в кастрюле, добавьте чечевицу и варите на среднем огне 20–25 минут до мягкости чечевицы.', 'Слейте лишнюю воду, добавьте мёд по вкусу и перемешайте.']}, {'title': 'Салат из чечевицы с мёдом и зеленью', 'steps': ['Отварите чечевицу в кипящей воде до готовности (около 20 минут).', 'Дайте чечевице остыть, затем добавьте немного мёда и мелко нарезанную зелень (укроп, петрушку).', 'Добавьте немного масла, перемешайте и подавайте.']}, {'title': 'Запечённая чечевица с мёдом', 'steps': ['Разогрейте духовку до 180 градусов Цельсия.', 'Выложите чечевицу в форму для запекания, добавьте мёд и масло, перемешайте.', 'Запекайте в духовке около 30 минут до золотистой корочки.']}]}
 
     recipes_for_kb = [(recipe.get("title"), i) for i, recipe in enumerate(result.get("recipes", {}))]
 
@@ -82,14 +82,23 @@ async def get_approval_product_list(cb: CallbackQuery, callback_data: ProductLis
 
 
 @router.callback_query(RecognizeState.waiting_for_choose_recipe, RecipesListCallback.filter())
-async def get_approval_product_list(cb: CallbackQuery, callback_data: RecipesListCallback, state: FSMContext):
+async def get_recipe_callback(cb: CallbackQuery, callback_data: RecipesListCallback, state: FSMContext):
+    if callback_data.back:
+        result = await state.get_value("recipes")
+        recipes_for_kb = [(recipe.get("title"), i) for i, recipe in enumerate(result)]
+
+        await cb.message.answer("Список рецептов: ", reply_markup=keyboard_recipes_builder(recipes_for_kb))
+        await cb.message.delete()
+        return
+
     recipe_num = callback_data.recipe_num
     recipes = await state.get_value("recipes")
     text = ""
     for i, recipe in enumerate(recipes[recipe_num].get("steps", [])):
         text += f"Шаг {i + 1}. {recipe}\n"
 
-    await cb.message.answer(text)
+    await cb.message.answer(text, reply_markup=keyboard_recipe_back_builder())
+    await cb.message.delete()
 
 
 @router.callback_query(ProductListCallback.filter())
