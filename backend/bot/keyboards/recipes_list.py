51fca56f3dfcd6a276c1c2c91ccb4a6726c89688
Flapchik <58729268+Flapchik@users.noreply.github.com>
2026-08-28 20:17:48 +0300

    feat (bot): добавить новый атрибут в recipes list callback (back) для возврата к списку рецептов

--- backend/bot/keyboards/recipes_list.py
+++ backend/bot/keyboards/recipes_list.py
@@ -5,12 +5,20 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
 class RecipesListCallback(CallbackData, prefix="recipes_list"):
-    recipe_num: int
+    recipe_num: int = 0
+    back: bool = False
 
 
 def keyboard_recipes_builder(recipes: list[tuple[str, int]]) -> InlineKeyboardMarkup:
     kb = InlineKeyboardBuilder()
     for recipe_name, recipe_num in recipes:
         kb.button(text=recipe_name, callback_data=RecipesListCallback(recipe_num=recipe_num))
+    kb.adjust(1)
+    return kb.as_markup()
+
+
+def keyboard_recipe_back_builder() -> InlineKeyboardMarkup:
+    kb = InlineKeyboardBuilder()
+    kb.button(text="К списку рецептов", callback_data=RecipesListCallback(back=True))
     kb.adjust(1)
     return kb.as_markup()
